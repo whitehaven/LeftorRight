9b9401677f5ade1ca5f53b92b37f6120db2c5b8e
whitehaven <accrist@gmail.com>
2015-09-01 18:34:14 -0600

    completed recursive left/right functions

--- LeftorRight.py
+++ LeftorRight.py
@@ -1,33 +1,43 @@
 __author__ = 'WhiteHaven'
 
+"""
+Note that each word must include a newline, including the last one.
+"""
+
 import sys
 
 leftHand = set()
-leftHand.update(['q','w','e','r','t','a','s','d','f','g','z','x','c','v','b'])
+leftHand.update(['q', 'w', 'e', 'r', 't', 'a', 's', 'd', 'f', 'g', 'z', 'x', 'c', 'v', 'b'])
 rightHand = set()
-rightHand.update(['y','u','i','o','p','h','j','k','l','n','m'])
+rightHand.update(['y', 'u', 'i', 'o', 'p', 'h', 'j', 'k', 'l', 'n', 'm'])
 
-print (sys.argv[0])
-print (sys.argv[1])
+
+def leftCheck(word, index):
+    if word[index] == '\n':  # if we made it all the way here already, we have not hit a bad char yet, so true
+        return True
+    elif word[index] in leftHand:
+        return leftCheck(word, index + 1)
+    else:
+        return False
+
+def rightCheck(word, index):
+    if word[index] == '\n':  # if we made it all the way here already, we have not hit a bad char yet, so true
+        return True
+    elif word[index] in rightHand:
+        return rightCheck(word, index + 1)
+    else:
+        return False
+
+print(sys.argv[0])
+print(sys.argv[1])
 
 dictionaryFile = open(sys.argv[1])
 
 trialWord = dictionaryFile.readline()
 
 while trialWord != "":
-
     trialWord.lower()
 
-    for letter in trialWord:
-        print(letter)
-        if letter in leftHand:
-            leftTrialWord = trialWord[1:]
-            for letterLeft in leftTrialWord:
-                if letterLeft in rightHand:
-                    print("Neither-hand: ", trialWord)
-                    break
+    print(trialWord,"   ",leftCheck(trialWord, 0),rightCheck(trialWord, 0))
 
-
-        elif letter in rightHand:
-            print("Right Hand Letter")
     trialWord = dictionaryFile.readline()
