8bf9914b5cf281daad5ef573cc44698a95dd5d73
whitehaven <accrist@gmail.com>
2015-09-02 17:39:06 -0600

    Added error checking for insufficient argument count with try-except block.

--- LeftorRight.py
+++ LeftorRight.py
@@ -6,60 +6,66 @@ Note that each word must include a newline, including the last one.
 
 import sys
 
-leftHand = set()
-leftHand.update(['q', 'w', 'e', 'r', 't', 'a', 's', 'd', 'f', 'g', 'z', 'x', 'c', 'v', 'b'])
-rightHand = set()
-rightHand.update(['y', 'u', 'i', 'o', 'p', 'h', 'j', 'k', 'l', 'n', 'm'])
+try:
+
+    leftHand = set()
+    leftHand.update(['q', 'w', 'e', 'r', 't', 'a', 's', 'd', 'f', 'g', 'z', 'x', 'c', 'v', 'b'])
+    rightHand = set()
+    rightHand.update(['y', 'u', 'i', 'o', 'p', 'h', 'j', 'k', 'l', 'n', 'm'])
 
 
-def leftCheck(word, index):
-    if word[index] == '\n':  # if we made it all the way here already, we have not hit a bad char yet, so true
-        return True
-    elif word[index] in leftHand:
-        return leftCheck(word, index + 1)
-    else:
-        return False
+    def leftCheck(word, index):
+        if word[index] == '\n':  # if we made it all the way here already, we have not hit a bad char yet, so true
+            return True
+        elif word[index] in leftHand:
+            return leftCheck(word, index + 1)
+        else:
+            return False
 
 
-def rightCheck(word, index):
-    if word[index] == '\n':  # if we made it all the way here already, we have not hit a bad char yet, so true
-        return True
-    elif word[index] in rightHand:
-        return rightCheck(word, index + 1)
-    else:
-        return False
+    def rightCheck(word, index):
+        if word[index] == '\n':  # if we made it all the way here already, we have not hit a bad char yet, so true
+            return True
+        elif word[index] in rightHand:
+            return rightCheck(word, index + 1)
+        else:
+            return False
 
 
-scriptName, dictionaryFileL, leftFileL, rightFileL = sys.argv
+    scriptName, dictionaryFileL, leftFileL, rightFileL = sys.argv
 
-dictionaryFile = open(dictionaryFileL)
+    dictionaryFile = open(dictionaryFileL)
 
-leftFile = open(leftFileL, 'w')
-rightFile = open(rightFileL, 'w')
+    leftFile = open(leftFileL, 'w')
+    rightFile = open(rightFileL, 'w')
 
-leftCount = 0
-rightCount = 0
-neitherCount = 0
-
-trialWord = dictionaryFile.readline()
-
-while trialWord != "":
-    trialWord.lower()
-
-    if leftCheck(trialWord,0):
-        leftCount += 1
-        leftFile.write(trialWord)
-    elif rightCheck(trialWord,0):
-        rightCount += 1
-        rightFile.write(trialWord)
-    else:
-        neitherCount += 1
+    leftCount = 0
+    rightCount = 0
+    neitherCount = 0
 
     trialWord = dictionaryFile.readline()
 
-totalCount = neitherCount + rightCount + leftCount
+    while trialWord != "":
+        trialWord.lower()
 
-print("Total Words: %d" % (totalCount))
-print("Right Words: %d (%.3f%%)" % (rightCount, float(rightCount) / float(totalCount) ) )
-print("Left Words: %d (%.3f%%)" % (leftCount, float(leftCount) / float(totalCount) ))
-print("Neither Words: %d (%.3f%%)" % (neitherCount, float(neitherCount) / float(totalCount) ))
+        if leftCheck(trialWord,0):
+            leftCount += 1
+            leftFile.write(trialWord)
+        elif rightCheck(trialWord,0):
+            rightCount += 1
+            rightFile.write(trialWord)
+        else:
+            neitherCount += 1
+
+        trialWord = dictionaryFile.readline()
+
+    totalCount = neitherCount + rightCount + leftCount
+
+    print("Total Words: %d" % (totalCount))
+    print("Right Words: %d (%.3f%%)" % (rightCount, float(rightCount) / float(totalCount) ) )
+    print("Left Words: %d (%.3f%%)" % (leftCount, float(leftCount) / float(totalCount) ))
+    print("Neither Words: %d (%.3f%%)" % (neitherCount, float(neitherCount) / float(totalCount) ))
+    
+except ValueError:
+    print("Insufficient arguments")
+    exit(-1)
